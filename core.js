81bbb4f3376f1451497836bfd97a5969efa1b0db
artmdbk <artyomdubko@gmail.com>
2016-10-20 17:11:58 +0300

    3

--- core.js
+++ core.js
@@ -4,10 +4,14 @@
      currentUrl = window.location.href,
       accessToken = window.location.href
  }
+ $(document).ready(function () {
+    if(window.location.href.indexOf("access_token") > -1) {
+		 vk.accessToken = newurl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
+	   $("#accessTokenId").html( "Access token:" + accessToken);
+    }
+});
  function getToken() {
 	  var newurl = "https://oauth.vk.com/authorize?client_id="+vk.appId+"&display=page&redirect_uri=https://artyomdubko.github.io/vk&scope=friends,wall,groups &response_type=token&v=5.59";
-	   window.location.replace(newurl);
-	   
-	   //vk.accessToken = newurl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
-	  // $("#accessTokenId").html( "Access token:" + accessToken);	   
+	   window.location.replace(newurl);   
+	    
  }
