3d37e525e1a3be7606f2050e996b8fc86c776aa7
artmdbk <artyomdubko@gmail.com>
2016-10-20 18:10:25 +0300

    sdf

--- core.js
+++ core.js
@@ -8,7 +8,7 @@
       vk.currentUrl = window.location.href;
 	  var newUrl=window.location.href;
       if (newUrl.indexOf("access_token") > -1) {
-          vk.accessToken = newUrl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
+          vk.accessToken = newUrl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];    //check if we redirected with with token
           $("#accessTokenId").html("Access token:" + vk.accessToken);
 		  $('.getting_auth').hide();
       }
@@ -17,7 +17,7 @@
 	  }
   });
 
-  function getToken() {
+  function redirectToGetToken() {   
       var redirectUrl = "https://oauth.vk.com/authorize?client_id=" + vk.appId + "&display=page&redirect_uri="+vk.currentUrl+"&scope=friends,wall,groups &response_type=token&v=5.59";
       window.location.replace(redirectUrl);
   }
