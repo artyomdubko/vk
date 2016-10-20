184d2bc9f2b59092b5416607fdf4e1ecdf242880
artmdbk <artyomdubko@gmail.com>
2016-10-20 17:34:09 +0300

    234

--- core.js
+++ core.js
@@ -11,9 +11,9 @@ $(document).ready(function() {
 		   $("#accessTokenId").html( "Access token:" + accessToken);
 		}
 	});
+ });
 	 function getToken() {
 		  var newurl = "https://oauth.vk.com/authorize?client_id="+vk.appId+"&display=page&redirect_uri=https://artyomdubko.github.io/vk&scope=friends,wall,groups &response_type=token&v=5.59";
 		   window.location.replace(newurl);   
 			
 	 }
- });
