19819201a759f0ed25e7247426c1a86d8d3cee0b
artmdbk <artyomdubko@gmail.com>
2016-10-20 18:27:01 +0300

    345

--- core.js
+++ core.js
@@ -1,14 +1,13 @@
   var vk = {
       appId: $('#AppId').val(),
       currentUrl: window.location.href,
+      currentUrlWithoutAnything: (location.protocol + '//' + location.host + location.pathname).replace(/\/$/, ""),
       accessToken: null
   }
   $(document).ready(function() {
-      vk.appId = $('#AppId').val();
-      vk.currentUrl = window.location.href;
-	  var newUrl=window.location.href;
-      if (newUrl.indexOf("access_token") > -1) {
-          vk.accessToken = newUrl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];    //check if we redirected with with token
+      vk.appId = $('#AppId').val(); 
+      if (vk.currentUrl.indexOf("access_token") > -1) {
+          vk.accessToken = vk.currentUrl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];    //check if we redirected with token
           $("#accessTokenId").html("Access token:" + vk.accessToken);
 		  $('.getting_auth').hide();
       }
@@ -18,6 +17,8 @@
   });
 
   function redirectToGetToken() {   
-      var redirectUrl = "https://oauth.vk.com/authorize?client_id=" + vk.appId + "&display=page&redirect_uri="+vk.currentUrl+"&scope=friends,wall,groups &response_type=token&v=5.59";
+      var redirectUrl = "https://oauth.vk.com/authorize?client_id=" + vk.appId + 
+	  "&display=page&redirect_uri="+ vk.currentUrlWithoutAnything +
+	  "&scope=friends,wall,groups&response_type=token&v=5.59";
       window.location.replace(redirectUrl);
   }
