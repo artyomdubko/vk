86390be6614523cb9f8cb398bddc08e8322b18db
artmdbk <artyomdubko@gmail.com>
2016-10-20 17:08:45 +0300

    2

--- core.js
+++ core.js
@@ -1,10 +1,13 @@
- var appId=$('#AppId
- var currentUrl = window.location.href;
- var accessToken = window.location.href;
  
+ var vk ={
+	 appId=$('#AppId').val(),
+     currentUrl = window.location.href,
+      accessToken = window.location.href
+ }
  function getToken() {
-	  var newurl = "https://oauth.vk.com/authorize?client_id="+appId+"&display=page&redirect_uri=https://artyomdubko.github.io/vk&scope=friends,wall,groups &response_type=token&v=5.59";
-	 var url = 'http://localhost:3000/_oauth/google#access_token=ya29.5HxuYol1Io8JLeGePDznbfkkwu_PC4uodKwG8_1clFYAn9AgdOV1WGpOTNQP3s76HAsn7Y4zWw&token_type=Bearer&expires_in=3600',
-      accessToken = url.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
-	  $("#accessTokenId").html( "Access token:" + accessToken);	   
+	  var newurl = "https://oauth.vk.com/authorize?client_id="+vk.appId+"&display=page&redirect_uri=https://artyomdubko.github.io/vk&scope=friends,wall,groups &response_type=token&v=5.59";
+	   window.location.replace(newurl);
+	   
+	   //vk.accessToken = newurl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
+	  // $("#accessTokenId").html( "Access token:" + accessToken);	   
  }
