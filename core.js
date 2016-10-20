cd98dde5739fc7a49721d945dc99d256fd319d9c
artmdbk <artyomdubko@gmail.com>
2016-10-20 18:09:11 +0300

    sdf

--- core.js
+++ core.js
@@ -10,10 +10,14 @@
       if (newUrl.indexOf("access_token") > -1) {
           vk.accessToken = newUrl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
           $("#accessTokenId").html("Access token:" + vk.accessToken);
+		  $('.getting_auth').hide();
       }
+	  else{
+		  $('.getting_auth').show();
+	  }
   });
 
   function getToken() {
-      var redirectUrl = "https://oauth.vk.com/authorize?client_id=" + vk.appId + "&display=page&redirect_uri=https://artyomdubko.github.io/vk&scope=friends,wall,groups &response_type=token&v=5.59";
+      var redirectUrl = "https://oauth.vk.com/authorize?client_id=" + vk.appId + "&display=page&redirect_uri="+vk.currentUrl+"&scope=friends,wall,groups &response_type=token&v=5.59";
       window.location.replace(redirectUrl);
   }
