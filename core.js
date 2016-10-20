4a4a67f95907ff095935d19314a567749b8d3aa3
artmdbk <artyomdubko@gmail.com>
2016-10-20 17:43:18 +0300

    234

--- core.js
+++ core.js
@@ -1,19 +1,18 @@
- 
-$(document).ready(function() { 
-	 var vk = {
-		 appId : $('#AppId').val(),
-		 currentUrl : window.location.href,
-		  accessToken : window.location.href
-	 }
-	 $(document).ready(function () {
-		if(window.location.href.indexOf("access_token") > -1) {
-			 vk.accessToken = newurl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
-		   $("#accessTokenId").html( "Access token:" + accessToken);
-		}
-	});
- });
-	 function getToken() {
-		  var newurl = "https://oauth.vk.com/authorize?client_id="+vk.appId+"&display=page&redirect_uri=https://artyomdubko.github.io/vk&scope=friends,wall,groups &response_type=token&v=5.59";
-		   window.location.replace(newurl);   
-			
-	 }
+  var vk = {
+      appId: $('#AppId').val(),
+      currentUrl: window.location.href,
+      accessToken: null
+  }
+  $(document).ready(function() {
+      vk.appId = $('#AppId').val();
+      vk.currentUrl = window.location.href;
+      if (window.location.href.indexOf("access_token") > -1) {
+          vk.accessToken = newurl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];
+          $("#accessTokenId").html("Access token:" + accessToken);
+      }
+  });
+
+  function getToken() {
+      var newurl = "https://oauth.vk.com/authorize?client_id=" + vk.appId + "&display=page&redirect_uri=https://artyomdubko.github.io/vk&scope=friends,wall,groups &response_type=token&v=5.59";
+      window.location.replace(newurl);
+  }
