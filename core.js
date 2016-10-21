e676519471ee44de78561396d95d8c8fcc006eaf
artmdbk <artyomdubko@gmail.com>
2016-10-21 09:44:15 +0300

    sdf

--- core.js
+++ core.js
@@ -9,16 +9,24 @@
       if (vk.currentUrl.indexOf("access_token") > -1) {
           vk.accessToken = vk.currentUrl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];    //check if we redirected with token
           $("#accessTokenId").html("Access token:" + vk.accessToken);
-		  $('.getting_auth').hide();
+		  AddOrRemoveDisableForArray($('.getting_auth').children(),true) 
       }
 	  else{
-		  $('.getting_auth').show();
+		  AddOrRemoveDisableForArray($('.getting_auth').children(),false) 
 	  }
   });
 
-  function redirectToGetToken() {   
+  function RedirectToGetToken() {   
       var redirectUrl = "https://oauth.vk.com/authorize?client_id=" + vk.appId + 
 	  "&display=page&redirect_uri="+ vk.currentUrlWithoutAnything +
 	  "&scope=friends,wall,groups&response_type=token&v=5.59";
       window.location.replace(redirectUrl);
   }
+
+   function AddOrRemoveDisableForArray(array,choise) {   //choise - bool 
+	  array.each(function() {
+	  $( this ).prop('disabled', choise);
+	  });
+   } 
+  
+});
