ee9ee2e94012cdd5e1c1db45342ee4b27e3e6a80
artmdbk <artyomdubko@gmail.com>
2016-10-21 13:08:29 +0300

    234

--- core.js
+++ core.js
@@ -12,9 +12,9 @@
       if (vk.currentUrl.indexOf("access_token") > -1) {
           vk.accessToken = vk.currentUrl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1]; //check if we redirected with token
           $("#accessTokenId").html("Access token:" + vk.accessToken);
-          addOrRemoveDisableForArray($('.getting_auth').children(), true)
+          addOrRemoveDisableForArray($('.getting-auth').children(), true)
       } else {
-          addOrRemoveDisableForArray($('.getting_auth').children(), false)
+          addOrRemoveDisableForArray($('.getting-auth').children(), false)
       }
   });
 
