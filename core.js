b3b2b8b77bc41fd0139e6b98beac4759ab4d9a9c
artmdbk <artyomdubko@gmail.com>
2016-10-21 12:02:08 +0300

    123

--- core.js
+++ core.js
@@ -7,69 +7,67 @@
       accessToken: ""
   }
   $(document).ready(function() {
-      vk.appId = $('#AppId').val(); 
-      vk.requestInterval = $('#requestInterval').val(); 
+      vk.appId = $('#AppId').val();
+      vk.requestInterval = $('#requestInterval').val();
       if (vk.currentUrl.indexOf("access_token") > -1) {
-          vk.accessToken = vk.currentUrl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1];    //check if we redirected with token
+          vk.accessToken = vk.currentUrl.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1]; //check if we redirected with token
           $("#accessTokenId").html("Access token:" + vk.accessToken);
-		  addOrRemoveDisableForArray($('.getting_auth').children(),true) 
+          addOrRemoveDisableForArray($('.getting_auth').children(), true)
+      } else {
+          addOrRemoveDisableForArray($('.getting_auth').children(), false)
       }
-	  else{
-		  addOrRemoveDisableForArray($('.getting_auth').children(),false) 
-	  }
   });
 
-  function redirectToGetToken() {   
-      var redirectUrl = "https://oauth.vk.com/authorize?client_id=" + vk.appId + 
-	  "&display=page&redirect_uri="+ vk.currentUrlWithoutAnything +
-	  "&scope=friends,wall,groups&response_type=token&v=5.59";
+  function redirectToGetToken() {
+      var redirectUrl = "https://oauth.vk.com/authorize?client_id=" + vk.appId +
+          "&display=page&redirect_uri=" + vk.currentUrlWithoutAnything +
+          "&scope=friends,wall,groups&response_type=token&v=5.59";
       window.location.replace(redirectUrl);
   }
 
-   function addOrRemoveDisableForArray(array,choise) {   //choise - bool 
-	  array.each(function() {
-	  $( this ).prop('disabled', choise);
-	  });
-   } 
-   
-   function startProcess() {   
-        vk.groupId = $("#wallId").val();
-         getGroupOrUserInfo();
-   }  
-   
-   function getGroupOrUserInfo() {     
-	     var params = {
-      "owner_id": vk.groupId
-		 }
-	  getJsonFromRequest("wall.get",params);
-  } 
-   
-   function getJsonFromRequest(methodName,params) {   
-       var parsedParametres = getParamsStringFromDictionary (params);
-	   var url =" https://api.vk.com/method/" + methodName+ "?" + parsedParametres;
-	   
-	   
-	  $.ajax({ 
-        url:url,
-        method: "GET",
-		async: false,
-        dataType : "jsonp",
-        success: function (data) { 
-		   
-        },
-        error: function (data) {
-            console.log(data.d);
-        }
-        });
-   } 
-   
-    function getParamsStringFromDictionary (paramsDictionary) {
-		var paramsString = "";
-		for (key  in paramsDictionary) { 
-		   var value = paramsDictionary[key];
-		   paramsString += key + "=" + value  + "&";
-		}
-		return paramsString
-}
-  
- 
+  function addOrRemoveDisableForArray(array, choise) { //choise - bool 
+      array.each(function() {
+          $(this).prop('disabled', choise);
+      });
+  }
+
+  function startProcess() {
+      vk.groupId = $("#wallId").val();
+      getGroupOrUserInfo();
+  }
+
+  function getGroupOrUserInfo() {
+      var params = {
+          "owner_id": vk.groupId
+      }
+      getJsonFromRequest("wall.get", params);
+  }
+
+  function getJsonFromRequest(methodName, params) {
+      var parsedParametres = getParamsStringFromDictionary(params);
+      var url = " https://api.vk.com/method/" + methodName + "?" + parsedParametres;
+
+
+      $.ajax({
+          url: url,
+          method: "GET",
+          async: false,
+          crossDomain: true,
+          dataType: 'jsonp',
+          success: function(data) {
+
+          },
+          error: function(data) {
+              console.log(data.d);
+          }
+      });
+  }
+
+  function getParamsStringFromDictionary(paramsDictionary) {
+      var paramsString = "";
+      for (key in paramsDictionary) {
+          var value = paramsDictionary[key];
+          paramsString += key + "=" + value + "&";
+      }
+      return paramsString
+  }
