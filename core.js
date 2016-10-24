ce1e478f1955f78a7dad7e46596f55a29f3ddabb
artmdbk <artyomdubko@gmail.com>
2016-10-24 18:40:18 +0300

    dfd

--- core.js
+++ core.js
@@ -68,18 +68,10 @@
       var paramsString = getParamsStringFromDictionary(params);
       var url = " https://api.vk.com/method/" + methodName + "?&access_token=" + vk.accessToken + "&v=5.59&" + paramsString;
       var response = "";
-      $.ajax({
-                url: url,
-                type: 'GET',
-				async: false,
-          crossDomain: true,
-          dataType: 'jsonp'
-            }).then(function (data) { 
-                console.log (data);
-            }).fail(function (err) { 
-                console.log (err);
-            });
-
+      $.when(ajaxRequest(url)).done(function(data) {
+          var response = data.response;
+          $("#requestsCount").html(parseInt($("#requestsCount").html()) + 1); //increment request count
+      }); 
       return response;
   }
 
@@ -94,7 +86,8 @@
           dataType: 'jsonp'
       });
   }
-
+  
+   
 
   function getParamsStringFromDictionary(paramsDictionary) {
       var paramsString = "";
