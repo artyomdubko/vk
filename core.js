b95ef4c1c656213fbdb6ca2a5c7ef07efda0d095
artmdbk <artyomdubko@gmail.com>
2016-10-25 13:33:02 +0300

    re

--- core.js
+++ core.js
@@ -2,6 +2,7 @@
       appId: $('#AppId').val(),
       groupId: "",
       postsCount: "",
+      comment: "",
       currentUrl: window.location.href,
       requestInterval: 500, //by default
       currentUrlWithoutAnything: (location.protocol + '//' + location.host + location.pathname).replace(/\/$/, ""),
@@ -34,81 +35,89 @@
 
   function startProcess() {
       vk.groupId = $("#wallId").val();
-      getGroupOrUserInfo();
-      var vkTimer = setInterval(function() {
-          var response = getJsonFromRequest("wall.get", params);
-          if (response > vk.postsCount) {
-              clearInterval(vkTimer);
-          }
-      }, vk.requestInterval);
+      vk.comment = $("#commentText").val();
+      getGroupOrUserInfo(vk.groupId); 
   }
 
 
-  function getGroupOrUserInfo() { //set to dropdown change
+  function getGroupOrUserInfo(groupId) { //set to dropdown change
       var params = {
-          "owner_id": vk.groupId
+          "owner_id": groupId
       }
       var response = "";
-	  doAnAjax("wall.get", params, function(data) {
-		  if (!data.error)
-		  {
-       response = data.response;
-      vk.postsCount = response.count;
-      $("#postsCount").html(vk.postsCount);
-      $("#lastPostValue").html(response.items[0].text.substring(0, 100)); //response[0] - posts count, response[1-...] - posts
-		  }
+      doAnAjax("wall.get", params, function(data) {
+          if (!data.error) {
+              response = data.response;
+			  if(response.count > vk.postsCount)
+			  {
+				  addCommentToPost (vk.groupId, response.items[0].id, vk.comment);
+			  }
 		  else {
-			    alert(data.error.error_code + data.error.error_msg);
+			  setTimeout(getGroupOrUserInfo(params), vk.requestInterval);
 		  }
-    });
+              vk.postsCount = response.count;
+              $("#postsCount").html(vk.postsCount);
+              $("#lastPostValue").html(response.items[0].text.substring(0, 100)); //response[0] - posts count, response[1-...] - posts 
+          } else {
+              alert(data.error.error_code + data.error.error_msg);
+          }
+      });
   }
-  function addCommentToPost() {
-      var params = {
-          "owner_id": vk.groupId
-      }
-      var response = getJsonFromRequest("wall.get", params);
-      vk.postsCount = response[0];
-      $("#postsCount").html(response[0]);
-      $("#lastPostValue").html(response[1].text.substring(0, 100)); //response[0] - posts count, response[1-...] - posts
+
+  function addCommentToPost(groupId, postId, commentText) { 
+				  var params = {
+					  "owner_id": groupId,
+					  "post_id": postId,
+					  "message": commentText
+				  }
+      var response = "";
+      doAnAjax("wall.createComment", params, function(data) {
+          if (!data.error) {
+              response = data.response;  
+          } else {
+              alert(data.error.error_code + data.error.error_msg);
+          }
+      });
+        
   }
- 
- 
-  function doAnAjax(methodName, params, callBack) {	  
+
+
+  function doAnAjax(methodName, params, callBack) {
       var paramsString = getParamsStringFromDictionary(params);
       var newUrl = " https://api.vk.com/method/" + methodName + "?&access_token=" + vk.accessToken + "&v=5.59&" + paramsString;
       var response = "";
       $("#requestsCount").html(parseInt($("#requestsCount").html()) + 1); //increment request count
-    $.ajax({
+      $.ajax({
           async: false,
           url: newUrl,
           method: methodName,
           async: false,
           crossDomain: true,
           dataType: 'jsonp',
-        success: function(data, textStatus, xhr) { 
-            return callBack( data );   
-        },
-        error: function(xhr, exception) {
-            //$('.rtnMsg').html("opps: " + textStatus + " : " + errorThrown);  
-            if (xhr.status === 0) {
-                alert('Not connect.\n Verify Network.');
-            } else if (xhr.status == 404) {
-                alert('Requested page not found. [404]');
-            } else if (xhr.status == 500) {
-                alert('Internal Server Error [500].');
-            } else if (exception === 'parsererror') {
-                alert('Requested JSON parse failed.');
-            } else if (exception === 'timeout') {
-                alert('Time out error.');
-            } else if (exception === 'abort') {
-                alert('Ajax request aborted.');
-            } else {
-                alert('Uncaught Error.\n' + xhr.responseText);
-            } 
-            return callBack ( myRtnA ); // return callBack() with myRtna
-        }
-    });
-   }
+          success: function(data, textStatus, xhr) {
+              return callBack(data);
+          },
+          error: function(xhr, exception) {
+              //$('.rtnMsg').html("opps: " + textStatus + " : " + errorThrown);  
+              if (xhr.status === 0) {
+                  alert('Not connect.\n Verify Network.');
+              } else if (xhr.status == 404) {
+                  alert('Requested page not found. [404]');
+              } else if (xhr.status == 500) {
+                  alert('Internal Server Error [500].');
+              } else if (exception === 'parsererror') {
+                  alert('Requested JSON parse failed.');
+              } else if (exception === 'timeout') {
+                  alert('Time out error.');
+              } else if (exception === 'abort') {
+                  alert('Ajax request aborted.');
+              } else {
+                  alert('Uncaught Error.\n' + xhr.responseText);
+              }
+              return callBack(myRtnA); // return callBack() with myRtna
+          }
+      });
+  }
 
   function getParamsStringFromDictionary(paramsDictionary) {
       var paramsString = "";
