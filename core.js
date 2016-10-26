e771b364670a940663f439040e54214525881ba3
artmdbk <artyomdubko@gmail.com>
2016-10-26 16:43:14 +0300

    1

--- core.js
+++ core.js
@@ -1,7 +1,7 @@
 
   VK.init(function() { 
   alert ("good");
-     VK.callMethod("showSettingsBox", 8192);
+     VK.callMethod("showSettingsBox", 8214);
   }, function() { 
   alert ("bad");
 }, '5.59'); 
