2346e02e89b34837569312eee0da113e66ab9e58
artmdbk <artyomdubko@gmail.com>
2016-10-26 15:20:35 +0300

    1

--- core.js
+++ core.js
@@ -2,10 +2,13 @@
   VK.init(function() { 
   alert ("good");
      VK.callMethod("showSettingsBox", 8214);
-	 VK.api("wall.post", {"owner_id": "-17633860","post_id": "657","message": "AAAAAA"}, function (data) {
-		alert("Post ID:" + data.response.post_id);
-		});
   }, function() { 
   alert ("bad");
 }, '5.59'); 
 
+
+function addComment (){	
+	 VK.api("wall.post", {"owner_id": "-17633860","post_id": "657","message": "AAAAAA"}, function (data) {
+		alert("Post ID:" + data.response.post_id);
+		});
+}
