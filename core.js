8543d4c2aa3fb869d2f5191c7d819c52d8d18fdc
artmdbk <artyomdubko@gmail.com>
2016-10-31 19:23:38 +0300

    23

--- core.js
+++ core.js
@@ -9,9 +9,9 @@
       accessToken: ""
   }
   var started = false;
-   var demo;
+  var demo;
   $(document).ready(function() {
-     $( document ).tooltip();
+      $(document).tooltip();
       demo = new Console({
           'element': '#debug',
           'width': 420,
@@ -33,8 +33,8 @@
 
   function checkUrlForToken(url) {
       if (url.indexOf("access_token") > -1) {
-          vk.accessToken = url.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1]; //check if we redirected with token
-          $("#accessTokenId").html("Access token: <p style='color:green;'>ЕСТЬ</p>" + );
+          vk.accessToken = url.match(/\#(?:access_token)\=([\S\s]*?)\&/)[1]; //check if we redirected with token 
+          $("#accessTokenId").html("<p style='color:green;'>ЕСТЬ</p>");
           addOrRemoveDisableForArray($('.getting-auth').children(), true)
       } else {
           addOrRemoveDisableForArray($('.getting-auth').children(), false)
@@ -42,7 +42,7 @@
   }
 
   function redirectToGetToken() {
-      vk.appId = $('#AppId').val();
+      //vk.appId = $('#AppId').val();
       var redirectUrl = "https://oauth.vk.com/authorize?client_id=" + vk.appId +
           "&display=page&redirect_uri=https://oauth.vk.com/blank.html" +
           "&scope=friends,wall,groups,messages&response_type=token&v=5.59";
@@ -56,18 +56,17 @@
   }
 
   function startProcess() {
-	  if (!started){
-		  vk.requestInterval = parseInt($('#requestInterval').val());
-		  vk.groupId = $("#wallId").val();
-		  vk.comment = $("#commentText").val();
-		  getGroupOrUserInfo(vk.groupId, true);
-		  $("#startButton").html('Stop');
-		  started = true;
-	  }
-	  else {
-		  $("#startButton").html('Start');
-		  started = false;		  
-	  }
+      if (!started) {
+          vk.requestInterval = parseInt($('#requestInterval').val());
+          vk.groupId = $("#wallId").val();
+          vk.comment = $("#commentText").val();
+          getGroupOrUserInfo(vk.groupId, true);
+          $("#startButton").html('Stop');
+          started = true;
+      } else {
+          $("#startButton").html('Start');
+          started = false;
+      }
   }
 
 
@@ -75,26 +74,24 @@
       var params = {
           "owner_id": groupId
       }
-	  var postId;
+      var postId;
       var response = "";
       doAnAjax("GET", "wall.get", params, function(data) {
           if (!data.error) {
               response = data.response;
-              if (!isFirstTime && response.count > vk.postsCount) {            //if new post added
-				  if (response.items[0].is_pinned!=1)                         
-				  {															  //check if new posts pinned. then we need items[1]
-				    postId = response.items[0].id;
-				  }
-				  else{
-				    postId = response.items[1].id;				  
-				  }
+              if (!isFirstTime && response.count > vk.postsCount) { //if new post added
+                  if (response.items[0].is_pinned != 1) { //check if new posts pinned. then we need items[1]
+                      postId = response.items[0].id;
+                  } else {
+                      postId = response.items[1].id;
+                  }
                   handleNewPost(response, vk.groupId, postId, vk.comment);
-              } else {															  //check group again if there isn't a new post
-				  if (started){                                                  //check if started clicked
-					  setTimeout(function() {
-						  getGroupOrUserInfo(groupId, false)
-					  }, vk.requestInterval);
-				  }
+              } else { //check group again if there isn't a new post
+                  if (started) { //check if started clicked
+                      setTimeout(function() {
+                          getGroupOrUserInfo(groupId, false)
+                      }, vk.requestInterval);
+                  }
               }
               vk.postsCount = response.count;
               $("#postsCount").html(vk.postsCount);
